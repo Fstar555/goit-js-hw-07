6b1fd97da132998639567d1ea59b8135ee0769f9
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-01-18 00:57:51 +0200

    update

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+"use strict";
+const categoriesList = document.querySelector("#categories");
+const categories = categoriesList.querySelectorAll(".item");
+const number = console.log(`Number of categories: ${categories.length}`);
+categories.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElements = category.querySelectorAll("li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
