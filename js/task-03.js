738a9667bb13da738eb2357207083bbbade57a85
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-01-19 01:47:02 +0200

    update task-03

---
+++ js/task-03.js
@@ -0,0 +1,11 @@
+"use strict";
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-outnput");
+
+const updateUserName = () => {
+  const updateName = nameInput.value.trim;
+
+  nameOutput.textContent = updateName ? updateName : "Anonymous";
+};
+
+nameInput.addEventListener("input", updateUserName);
