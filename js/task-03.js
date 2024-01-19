5f97122cb8931a8249a4787659c284628f5503e0
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-01-19 02:33:29 +0200

    update task-03, 04

--- js/task-03.js
+++ js/task-03.js
@@ -3,7 +3,7 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-outnput");
 
 const updateUserName = () => {
-  const updateName = nameInput.value.trim;
+  const updateName = nameInput.value.trim();
 
   nameOutput.textContent = updateName ? updateName : "Anonymous";
 };
---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const loginForm = document.querySelector(".login-form");
+const emailIntput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
+
+const form = (event) => {
+  event.preventDefault();
+
+  const emailValue = emailIntput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log(
+      `Email:${emailValue}
+            Password:${passwordValue}`,
+      loginForm(reset)
+    );
+  }
+};
+
+loginForm.addEventListener("submit", form);
